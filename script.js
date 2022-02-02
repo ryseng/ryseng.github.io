461fc474f2b1be1d4f3c655c2f5c90becdcfd259
ryseng <ry.seng@yahoo.com>
2022-02-02 09:27:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function calculateWorkingSet()
 {
         num1 = document.getElementById("oneRepMax").value;
         num2 = document.getElementById("setPercent").value;
-        document.getElementById("result").innerHTML = num1 * num2;
+        document.getElementById("result").innerHTML = (((num1*(num2*.01))-45)/2);
 }
